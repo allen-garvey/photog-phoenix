b0d73be9fe841197979d0945ac23043e6096344b
Allen Garvey <allen.garvey@gmail.com>
2018-12-01 22:03:40 -0500

    Update postgrex version

--- mix.exs
+++ mix.exs
@@ -37,7 +37,7 @@ defmodule Photog.Mixfile do
       {:phoenix_pubsub, "~> 1.0"},
       {:phoenix_ecto, "~> 4.0"},
       {:ecto_sql, "~> 3.0.3"},
-      {:postgrex, ">= 0.13.5"},
+      {:postgrex, ">= 0.14.1"},
       {:phoenix_html, "~> 2.10"},
       {:phoenix_live_reload, "~> 1.0", only: :dev},
       {:gettext, "~> 0.11"},
