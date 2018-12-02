47b25b065ec603ae7857795210e9ce2038ab8694
Allen Garvey <allen.garvey@gmail.com>
2018-12-01 21:59:31 -0500

    Update phoenix and cowboy versions

--- mix.exs
+++ mix.exs
@@ -33,14 +33,15 @@ defmodule Photog.Mixfile do
   # Type `mix help deps` for examples and options.
   defp deps do
     [
-      {:phoenix, "~> 1.3.3"},
+      {:phoenix, "~> 1.4"},
       {:phoenix_pubsub, "~> 1.0"},
       {:phoenix_ecto, "~> 3.2"},
       {:postgrex, ">= 0.13.5"},
       {:phoenix_html, "~> 2.10"},
       {:phoenix_live_reload, "~> 1.0", only: :dev},
       {:gettext, "~> 0.11"},
-      {:cowboy, "~> 1.0"},
+      {:cowboy, "~> 2.6"},
+      {:plug_cowboy, "~> 2.0"},
       {:poison, "~> 3.0"},
     ]
   end
