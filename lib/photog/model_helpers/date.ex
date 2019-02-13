819bebc3bf2c044fe95549bf6256f3d932a267b7
Allen Garvey <allen.garvey@gmail.com>
2019-02-13 10:03:33 -0500

    Fix current date being used as import import_time default instead of current datetime

--- lib/photog/model_helpers/date.ex
+++ lib/photog/model_helpers/date.ex
@@ -2,12 +2,12 @@ defmodule Photog.ModelHelpers.Date do
     import Ecto.Changeset
 
   @doc """
-  Adds default date as today if nil
+  Adds default datetime as now if nill
   """
-  def default_date_today(changeset, attribute_key) do
-    date_value = get_field(changeset, attribute_key)
-    if is_nil(date_value) do
-      change(changeset, %{attribute_key => Date.utc_today})
+  def default_datetime_now(changeset, attribute_key) do
+    if is_nil(get_field(changeset, attribute_key)) do
+      now = DateTime.utc_now() |> DateTime.truncate(:second)
+      change(changeset, %{attribute_key => now})
     else
       changeset
     end
