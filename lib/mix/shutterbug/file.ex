69e706c59044fd4e80014ee925751b5488ae603d
Allen Garvey <allen.garvey@gmail.com>
2019-02-13 11:38:34 -0500

    Edit resize image command to use size on shortest side

--- lib/mix/shutterbug/file.ex
+++ lib/mix/shutterbug/file.ex
@@ -25,11 +25,11 @@ defmodule Photog.Shutterbug.File do
   end
 
   @doc """
-  Resizes image to given dimension on longest side using imagemagick
+  Resizes image to given dimension on shortest side using imagemagick
   Resize on largest side from: https://www.imagemagick.org/discourse-server/viewtopic.php?t=13175
   """
   def resize_image(image_source_path, image_destination_path, size) when is_integer(size) do
-    case System.cmd("convert", [image_source_path, "-resize", "#{size}>", "-quality", "80%", image_destination_path]) do
+    case System.cmd("convert", [image_source_path, "-resize", "#{size}^>", "-quality", "80%", image_destination_path]) do
       {_, 0} -> true
       _      -> Shutterbug.exit_with_error("Error creating #{image_destination_path} using convert", :error_creating_thumbnail)
     end
