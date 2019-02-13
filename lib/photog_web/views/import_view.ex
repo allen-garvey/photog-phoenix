69dd134e9f6e27b6068a1ad74eb31cda0e78e235
Allen Garvey <allen.garvey@gmail.com>
2019-02-13 08:37:06 -0500

    Add images and string representation of import time to json returned for import resource

--- lib/photog_web/views/import_view.ex
+++ lib/photog_web/views/import_view.ex
@@ -1,17 +1,30 @@
 defmodule PhotogWeb.ImportView do
   use PhotogWeb, :view
   alias PhotogWeb.ImportView
+  alias PhotogWeb.ViewHelpers.DateHelpers
 
   def render("index.json", %{imports: imports}) do
-    %{data: render_many(imports, ImportView, "import.json")}
+    %{data: render_many(imports, ImportView, "import_excerpt.json")}
   end
 
   def render("show.json", %{import: import}) do
     %{data: render_one(import, ImportView, "import.json")}
   end
 
+  def render("import_excerpt.json", %{import: import}) do
+    %{
+      id: import.id,
+      import_time: import.import_time,
+      name: DateHelpers.datetime_to_us_format(import.import_time),
+    }
+  end
+
   def render("import.json", %{import: import}) do
-    %{id: import.id,
-      import_time: import.import_time}
+    %{
+      id: import.id,
+      import_time: import.import_time,
+      name: DateHelpers.datetime_to_us_format(import.import_time),
+      images: Enum.map(import.images, &PhotogWeb.ImageView.image_full_to_map/1),
+    }
   end
 end
