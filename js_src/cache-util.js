52fa0dc250e1b883ad082a701c81ea7509a0bbd7
Allen Garvey <allen.garvey@gmail.com>
2019-02-14 11:09:27 -0500

    Refactor cache-util script to use request helper fetchJson function

--- js_src/cache-util.js
+++ js_src/cache-util.js
@@ -1,13 +1,12 @@
+import { fetchJson } from './request-helpers.js';
+
 function fetchIntoCache(url, cacheMap, mapId){
     if(cacheMap.has(mapId)){
         return new Promise((resolve)=>{
             resolve(cacheMap.get(mapId));
         });
     }
-    return fetch(url).then((res)=>{
-        return res.json();
-    }).then((json)=>{
-        const data = json.data;
+    return fetchJson(url).then((data)=>{
         cacheMap.set(mapId, data);
         return data;
     });
