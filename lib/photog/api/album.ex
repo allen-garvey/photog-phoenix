c9eb07e5e9163d40442f9e00152f6f82e2d52f11
Allen Garvey <allen.garvey@gmail.com>
2019-02-16 09:01:52 -0500

    Drop folders table

--- lib/photog/api/album.ex
+++ lib/photog/api/album.ex
@@ -5,13 +5,11 @@ defmodule Photog.Api.Album do
 
   schema "albums" do
     field :apple_photos_id, :integer
-    field :folder_order, :integer
     field :name, :string
 
     timestamps()
 
     belongs_to :cover_image, Photog.Api.Image
-    belongs_to :folder, Photog.Api.Folder
 
     has_many :album_images, Photog.Api.AlbumImage
     many_to_many :images, Photog.Api.Image, join_through: "album_images"
@@ -23,10 +21,9 @@ defmodule Photog.Api.Album do
   @doc false
   def changeset(album, attrs) do
     album
-    |> cast(attrs, [:apple_photos_id, :name, :folder_order, :folder_id, :cover_image_id])
-    |> validate_required([:apple_photos_id, :name, :folder_order, :folder_id, :cover_image_id])
+    |> cast(attrs, [:apple_photos_id, :name, :cover_image_id])
+    |> validate_required([:apple_photos_id, :name, :cover_image_id])
     |> unique_constraint(:apple_photos_id)
-    |> assoc_constraint(:folder)
     |> assoc_constraint(:image)
     |> assoc_constraint(:cover_image)
   end
