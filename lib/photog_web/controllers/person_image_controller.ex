328fd4b319dce3f27065a532e10515f10532a588
Allen Garvey <allen.garvey@gmail.com>
2019-02-17 10:41:45 -0500

    Add person image batch create api route

--- lib/photog_web/controllers/person_image_controller.ex
+++ lib/photog_web/controllers/person_image_controller.ex
@@ -11,6 +11,35 @@ defmodule PhotogWeb.PersonImageController do
     render(conn, "index.json", person_images: person_images)
   end
 
+  @doc """
+  Batch add person_ids to each given image_id in image_ids
+  """
+  def create(conn, %{"image_ids" => image_ids, "person_ids" => person_ids}) do
+    {total_added, total_errors} =
+      Enum.reduce(image_ids, {[], []}, fn image_id, {total_added, total_errors} ->
+        {persons_added, errors} =
+          Enum.reduce(person_ids, {[], []}, fn person_id, {persons_added, errors} ->
+            person_image_params = %{"image_id" => image_id, "person_id" => person_id}
+            case Api.create_person_image(person_image_params) do
+              {:ok, %PersonImage{} = _person_image} -> { [person_image_params | persons_added], errors }
+              {:error, _changeset}                  -> { persons_added, [ person_image_params | errors] }
+
+            end
+          end)
+        {persons_added ++ total_added, errors ++ total_errors}
+      end)
+
+    conn
+    |> put_view(PhotogWeb.GenericView)
+    |> (&(
+      if Enum.empty?(total_errors) do
+        render(&1, "ok.json", message: total_added)
+      else
+        render(&1, "mixed_response.json", message: total_added, error: total_errors)
+      end
+    )).()
+  end
+
   def create(conn, %{"person_image" => person_image_params}) do
     with {:ok, %PersonImage{} = person_image} <- Api.create_person_image(person_image_params) do
       conn
