683f9c696c00069c81c8343c3f02590c30441c40
Allen Garvey <allengarvey@hotmail.com>
2019-02-19 18:15:06 -0500

    Move resource header styles to separate sass partial

--- js_src/components/resource-header.vue
+++ js_src/components/resource-header.vue
@@ -1,6 +1,6 @@
 <template>
-    <div class="spread-content thumbnail-list-heading">
-        <div><h2 class="thumbnail-list-title" v-if="title">{{title}}</h2></div>
+    <div class="spread-content resource-header-container">
+        <div><h2 class="resource-header-title" v-if="title">{{title}}</h2></div>
         <div class="pull-right" v-if="newItemLink || editItemLink">
             <router-link :to="newItemLink" class="btn btn-success" v-if="newItemLink">New</router-link>
             <router-link :to="editItemLink" class="btn btn-outline-dark" v-if="editItemLink">Edit</router-link>
