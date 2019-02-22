32853d85f8fd5e4fdc12a72f64f36e49879a9cb0
Allen Garvey <allengarvey@hotmail.com>
2019-02-22 09:13:52 -0500

    Display tiny list of thumbnails below import on import index vue

--- js_src/components/imports-index.vue
+++ js_src/components/imports-index.vue
@@ -6,18 +6,28 @@
                 <router-link :to="showRouteFor(item)">
                     {{titleFor(item)}}
                 </router-link>
+                <ul class="imports-index-mini-thumbnail-list">
+                    <li v-for="image in item.images" :key="image.id">
+                        <img :src="thumbnailUrlFor(image)"/>
+                    </li>
+                </ul>
             </li>
         </ul>
     </main>
 </template>
 
 <script>
+import { thumbnailUrlFor } from '../image.js';
 import textListBuilder from './components-base/text-list-base.js';
 
 const component = textListBuilder();
 
 component.methods.titleFor = function(item){
     return `${item.name} (${item.images_count})`;
+};
+
+component.methods.thumbnailUrlFor = function(image){
+    return thumbnailUrlFor(image.mini_thumbnail_path);
 };
 
 export default component;
