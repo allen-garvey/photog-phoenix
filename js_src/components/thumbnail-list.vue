2a0b8b8d5632d90cc535b84c6439058a834a50c4
Allen Garvey <allengarvey@hotmail.com>
2019-02-22 19:53:22 -0500

    Edit thumbnail list to take prop for page title

--- js_src/components/thumbnail-list.vue
+++ js_src/components/thumbnail-list.vue
@@ -1,6 +1,6 @@
 <template>
     <main class="main container">
-        <Resource-Header :title="model.name" :editItemLink="editItemLink" :newItemLink="newItemLink" :description="model.description" />
+        <Resource-Header :title="pageTitle ? pageTitle : model.name" :editItemLink="editItemLink" :newItemLink="newItemLink" :description="model.description" />
         <div class="thumbnail-filter-controls-container" :class="{invisible: isCurrentlyBatchSelect}">
             <fieldset v-if="enableHasAlbumFilter">
                 <legend>Album</legend>
@@ -124,6 +124,9 @@ export default {
         },
         editItemLink: {
             type: Object,
+        },
+        pageTitle: {
+            type: String,
         },
         enableHasAlbumFilter: {
             type: Boolean,
