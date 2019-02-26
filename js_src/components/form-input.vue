eeafc91c11beb105e23a260ecbe5263e813aa511
Allen Garvey <allengarvey@hotmail.com>
2019-02-26 08:40:05 -0500

    Create form input vue component

---
+++ js_src/components/form-input.vue
@@ -0,0 +1,46 @@
+<template>
+    <div class="form-group">
+        <label :for="id">{{label}}</label>
+        <input :id="id" class="form-control" :type="inputType" v-model="value" />
+        <Form-Field-Errors :errors="errors" />
+    </div>
+</template>
+
+<script>
+import FormFieldErrors from './form-field-errors.vue';
+
+export default {
+    name: 'Form-Input',
+    props: {
+        id: {
+            type: String,
+            required: true,
+        },
+        inputType: {
+            type: String,
+            default: 'text',
+        },
+        label: {
+            type: String,
+            required: true,
+        },
+        errors: {
+            type: Array
+        },
+    },
+    components: {
+        'Form-Field-Errors': FormFieldErrors,
+    },
+    data() {
+        return {
+            value: '',
+        }
+    },
+    watch: {
+        value(newValue){
+            this.$emit('input', newValue);
+        },
+    },
+}
+</script>
+
