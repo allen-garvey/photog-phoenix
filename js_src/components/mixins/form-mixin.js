c424382c029c2073388344e7917d4f080e400143
Allen Garvey <allengarvey@hotmail.com>
2019-02-26 08:50:32 -0500

    Create vue component for form section

--- js_src/components/mixins/form-mixin.js
+++ js_src/components/mixins/form-mixin.js
@@ -1,9 +1,11 @@
+import FormSection from '../base/form-section.vue';
 import FormInput from '../form-input.vue';
 
 export function formMixinBuilder(){
     return {
         components: {
             'Form-Input': FormInput,
+            'Form-Section': FormSection,
         },
     };
 }
