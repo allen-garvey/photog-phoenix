3766ea78d420dc3ff0b8925990dd0ec22148386d
Allen Garvey <allengarvey@hotmail.com>
2019-02-27 08:14:25 -0500

    Refactor album form vue to use form section component

--- js_src/components/album-form.vue
+++ js_src/components/album-form.vue
@@ -1,9 +1,6 @@
 <template>
-    <main class="main container" v-if="isInitialLoadComplete">
-        <div>
-            <h1>{{headingText}}</h1>
-        </div>
-        <div>
+    <Form-Section :heading="headingText" :back-link="backLink" :save="save" v-if="isInitialLoadComplete">
+        <template v-slot:inputs>
             <div class="form-group">
                 <label :for="idForField('name')">Name</label>
                 <input :id="idForField('name')" class="form-control" type="text" v-model="album.name" />
@@ -31,13 +28,8 @@
                 </label>
                 <Form-Field-Errors :errors="[errors['cover_image'], errors['cover_image_id']]" />
             </fieldset>
-
-            <div class="spread-content">
-                <router-link :to="backLink" class="btn btn-outline-dark">Cancel</router-link>
-                <button class="btn btn-success" @click="save()">Save</button>
-            </div>
-        </div>
-    </main>
+        </template>
+    </Form-Section>
 </template>
 
 <script>
