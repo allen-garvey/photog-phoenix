6771fa82dcb0f69272bfdc9654777361912d56fb
Allen Garvey <allen.garvey@gmail.com>
2019-03-03 08:17:51 -0500

    Add vue infinite scroll for text list vue component

--- js_src/components/base/text-list.vue
+++ js_src/components/base/text-list.vue
@@ -1,8 +1,8 @@
 <template>
     <main class="main container">
         <Resource-Header :title="title" :editItemLink="editItemLink" :newItemLink="newItemLink" />
-        <ul class="text-list">
-            <li v-for="(item, i) in itemsList" :key="i">
+        <ul class="text-list" v-infinite-scroll="showMoreItems" infinite-scroll-distance="40" infinite-scroll-disabled="isInfiniteScrollDisabled">
+            <li v-for="(item, i) in itemsListExcerpt" :key="i">
                 <slot name="item" :item="item" :index="i"></slot>
             </li>
         </ul>
@@ -10,7 +10,11 @@
 </template>
 
 <script>
+import infiniteScroll from 'vue-infinite-scroll';
 import ReasourceHeader from '../resource-header.vue';
+
+//number of items to show each time infinite scroll is called
+const ITEM_CHUNK_LENGTH = 20;
 
 export default {
         name: 'Text-List',
@@ -32,6 +36,33 @@ export default {
         },
         components: {
             'Resource-Header': ReasourceHeader,
+        },
+        directives: {
+            infiniteScroll,
+        },
+        data(){
+            return {
+                itemsListExcerptLength: ITEM_CHUNK_LENGTH,
+            };
+        },
+        computed: {
+            itemsListExcerpt(){
+                return this.itemsList.slice(0, this.itemsListExcerptLength);
+            },
+            isInfiniteScrollDisabled(){
+               return this.itemsListExcerptLength >= this.itemsList.length;
+            },
+        },
+        watch: {
+        '$route'(to, from){
+            //reset itemsListExcerptLength
+            this.itemsListExcerptLength = ITEM_CHUNK_LENGTH;
+        },
+    },
+        methods: {
+            showMoreItems(){
+                this.itemsListExcerptLength += ITEM_CHUNK_LENGTH;
+            },
         },
     };
 </script>
