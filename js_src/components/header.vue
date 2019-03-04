e89b3cfbb9335c95b59064c0d07016b8639f0b12
Allen Garvey <allengarvey@hotmail.com>
2019-03-04 08:15:04 -0500

    Add link to last import in header

--- js_src/components/header.vue
+++ js_src/components/header.vue
@@ -9,6 +9,7 @@
                 <li><router-link :to="{name: 'imageFavoritesIndex'}">Favorites</router-link></li>
                 <li><router-link :to="{name: 'imagesNotInAlbumIndex'}">Uncategorized Images</router-link></li>
                 <li><router-link :to="{name: 'importsIndex'}">Imports</router-link></li>
+                <li><router-link :to="{name: 'importsShowLast'}">Last Import</router-link></li>
             </ul>
         </nav>
     </header>
